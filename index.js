9bed41d95d62af4615745a978d605d0bed7655c9
FX Wood <fxwood2@gmail.com>
2018-11-20 21:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var beatles = [];
-  for (let i = 0; i < musicians.length || instruments.length; i++) {
+  for (let i = 0; musicians.length > 0; i++) {
     beatles.push(musicians[i] + "plays" + instruments[i])
   return beatles
   }
