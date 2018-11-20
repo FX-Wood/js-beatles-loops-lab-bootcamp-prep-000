95dd6de6fd3aa7c6727a6829f325c2ac713439da
FX Wood <fxwood2@gmail.com>
2018-11-20 21:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
+var beatles = []
 
 function theBeatlesPlay(musicians, instruments) {
-  var beatles = [];
   for (let i = 0; musicians.length > 0; i++) {
     beatles.push(musicians[i] + " plays " + instruments[i])
   return beatles
