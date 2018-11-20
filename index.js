9bcc10e19910dbc1a1cc3a04589943ccb468c6e3
FX Wood <fxwood2@gmail.com>
2018-11-20 21:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // add solution here
-var beatles = []
+var beatles = [];
 
 function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < 4 ; i++) {
-    beatles.push(musicians[i] + " plays " + instruments[i])
+    beatles.push(musicians[i] + " plays " + instruments[i]);
   }
-  return beatles
+  return beatles;
+}
+
+var johnLennonExclamations = [];
+
+function johnLennonFacts(facts) {
+  while (facts.length > 0) {
+    johnLennonExclamations.push(facts[0] + " !!!")
+  }
 }
